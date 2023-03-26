f29b74a39322edbf9267803710c3e359635b97a2
wklili <bitcoinnftpizzalab@gmail.com>
2023-03-26 19:45:34 +0800

    update

--- src/watch.js
+++ src/watch.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const fs = require("fs");
 const pizzasList = require("../assets/pizzasList.json");
 
-const lowestOffsetNum = 589615;
+const lowestOffsetNum = 599544;
 let currentOffsetNum = lowestOffsetNum;
 
 const ORD_URL = "https://ordapi.xyz";
